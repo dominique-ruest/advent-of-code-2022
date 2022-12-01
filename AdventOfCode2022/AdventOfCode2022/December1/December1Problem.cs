4ff5b74d2df226766335e306b2e2925abfb3ac01
Dominique Ruest <dominique.ruest@gmail.com>
2022-12-01 14:34:33 -0500

    reduce indentation

--- AdventOfCode2022/AdventOfCode2022/December1/December1Problem.cs
+++ AdventOfCode2022/AdventOfCode2022/December1/December1Problem.cs
@@ -1,36 +1,35 @@
-﻿namespace AdventOfCode2022.December1
+﻿namespace AdventOfCode2022.December1;
+
+public class December1Problem
 {
-    public class December1Problem
+    private readonly List<int> elfTotalCalories = new() { 0 };
+
+    public December1Problem(string[] input)
     {
-        private readonly List<int> elfTotalCalories = new() { 0 };
+        ParseElvesTotalCalories(input);
+    }
 
-        public December1Problem(string[] input)
-        {
-            ParseElvesTotalCalories(input);
-        }
+    public int GetCaloriesOfHighestCarryingElf()
+    {
+        return elfTotalCalories.OrderByDescending(p => p).First();
+    }
 
-        public int GetCaloriesOfHighestCarryingElf()
-        {
-            return elfTotalCalories.OrderByDescending(p => p).First();
-        }
+    public int GetCaloriesOfThreeHighestCarryingElves()
+    {
+        return elfTotalCalories.OrderByDescending(p => p).Take(3).Sum();
+    }
 
-        public int GetCaloriesOfThreeHighestCarryingElves()
+    private void ParseElvesTotalCalories(string[] input)
+    {
+        foreach (var line in input)
         {
-            return elfTotalCalories.OrderByDescending(p => p).Take(3).Sum();
-        }
-
-        private void ParseElvesTotalCalories(string[] input)
-        {
-            foreach (var line in input)
+            if (line == string.Empty)
             {
-                if (line == string.Empty)
-                {
-                    elfTotalCalories.Add(0);
-                }
-                else
-                {
-                    elfTotalCalories[^1] += int.Parse(line);
-                }
+                elfTotalCalories.Add(0);
+            }
+            else
+            {
+                elfTotalCalories[^1] += int.Parse(line);
             }
         }
     }
