5e288fcb6b37baf6d45b2d2b385ea217a7b37b2d
Dominique Ruest <dominique.ruest@gmail.com>
2022-12-04 10:38:39 -0500

    Cleanup Day4 program

--- AdventOfCode2022/AdventOfCode2022/December4/Day4Program.cs
+++ AdventOfCode2022/AdventOfCode2022/December4/Day4Program.cs
@@ -7,26 +7,17 @@
             Console.WriteLine("Day4");
 
             var day4Inputs = File.ReadAllLines("December4/input.txt");
-
-            var fullyContainedCount = day4Inputs.Where(input =>
+            var pairs = day4Inputs.Select(input =>
             {
                 var intervals = input.Split(',');
                 var elf1Assignment = Interval.FromInput(intervals[0]);
                 var elf2Assignment = Interval.FromInput(intervals[1]);
+                return (elf1Assignment, elf2Assignment);
+            }).ToList();
 
-                return elf1Assignment.Contains(elf2Assignment) || elf2Assignment.Contains(elf1Assignment);
-            })
-            .Count();
+            var fullyContainedCount = pairs.Count((pair) => pair.elf1Assignment.Contains(pair.elf2Assignment) || pair.elf2Assignment.Contains(pair.elf1Assignment));
 
-            var overlapsCount = day4Inputs.Where(input =>
-            {
-                var intervals = input.Split(',');
-                var elf1Assignment = Interval.FromInput(intervals[0]);
-                var elf2Assignment = Interval.FromInput(intervals[1]);
-
-                return elf1Assignment.Overlaps(elf2Assignment);
-            })
-            .Count();
+            var overlapsCount = pairs.Count((pair) => pair.elf1Assignment.Overlaps(pair.elf2Assignment));
 
             Console.WriteLine("Assignment pairs with one pair fully containing the other: " + fullyContainedCount);
             Console.WriteLine("Assignment pairs with overlaps: " + overlapsCount);
